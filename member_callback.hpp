e31f666e52263b6a4db816b5aaf3e33887e1493b
Shino <anonymous.from.applecity@gmail.com>
2016-12-05 23:43:32 +0600

    added namespace around member_callback

--- member_callback.hpp
+++ member_callback.hpp
@@ -2,39 +2,44 @@
 #include <memory>
 #include <utility>
 
-template <typename T, typename Func>
-class member_callback;
-
-template <typename T, typename R, typename ... ArgTypes>
-class member_callback<T, R(ArgTypes...)>
+namespace shino
 {
-	std::shared_ptr<T> object;
-	R(T::* callback)(ArgTypes ...);
-public:
-    using function_pointer = R(T::*)(ArgTypes ...);
+    template<typename T, typename Func>
+    class member_callback;
 
-	member_callback(const std::shared_ptr<T>& ptr, R(T::* function)(ArgTypes...)) :
-		object(ptr),
-		callback(function)
-	{}
+    template<typename T, typename R, typename ... ArgTypes>
+    class member_callback<T, R(ArgTypes...)>
+    {
+        std::shared_ptr<T> object;
 
-	member_callback(std::shared_ptr<T>&& ptr, R(T::* function)(ArgTypes...)) :
-		object(std::move(ptr)),
-		callback(function)
-	{}
+        R (T::* callback)(ArgTypes ...);
 
-	R operator()(ArgTypes&& ... args)
-	{
-		return ((*object).*callback)(std::forward<ArgTypes>(args)...);
-	}
+    public:
+        using function_pointer = R(T::*)(ArgTypes ...);
 
-	std::shared_ptr<T> get()
-	{
-		return object;
-	}
+        member_callback(const std::shared_ptr<T> &ptr, R(T::* function)(ArgTypes...)) :
+                object(ptr),
+                callback(function)
+        {}
 
-    function_pointer get_function()
-	{
-		return callback;
-	}
-};
+        member_callback(std::shared_ptr<T> &&ptr, R(T::* function)(ArgTypes...)) :
+                object(std::move(ptr)),
+                callback(function)
+        {}
+
+        R operator()(ArgTypes &&... args)
+        {
+            return ((*object).*callback)(std::forward<ArgTypes>(args)...);
+        }
+
+        std::shared_ptr<T> get()
+        {
+            return object;
+        }
+
+        function_pointer get_function()
+        {
+            return callback;
+        }
+    };
+}
