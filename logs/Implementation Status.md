a59c2e214a82d2ef5c23ce7e2c064313058a8fa7
Shino <anonymous.from.applecity@gmail.com>
2017-02-25 00:50:16 +0600

    Added tests for transform_iterator

--- logs/Implementation Status.md
+++ logs/Implementation Status.md
@@ -28,17 +28,11 @@ Every function, class, or any other code should get through here before being me
  
  - <h4> Tests: <font color="green">good</font>, but could be better</h4>
  
-   There is a test or two, but all of them are premade and non randomized, so the code might be silently hiding bugds inside.
- 
  - <h4> Documentation: <font color="green">good</font></h4>
  
    Documentation provides solid start for users.
  
- ##transform_iterator (ex preprocessing_iterator)
- 
- - <h4>Specification:</h4>
- 
-    >Given `Functor functor` that conforms `Callable`, `Iterator iterator` that conforms `OutputIterator` create a new iterator that instead of assigning `value_type`s directly, apply `functor` first and pass the result into underlying iterator.
+ ##transform_iterator
     
  - <h4>Code review:</h4>
  
@@ -50,23 +44,16 @@ Every function, class, or any other code should get through here before being me
   
    Bugfix: post increment was pre incrementing. Fixed. Added full support for Iterator and OutputIterator concept.
    
-   Bugfix: `transformer<>()` was incorrectly deducing types which lead to storing functor as reference. Now the transformoer will strip off references and constness.
+   Bugfix: `transformer<>()` was incorrectly deducing types which lead to storing functor as reference. 
+   Now the transformer will strip off references and constness.
   
-  - <h4> Tests: <font color=red>very poor tests</font></h4>
-  
-  There is a test or two, but all of them are premade and non randomized, so the code might be silently hiding bugs inside.
+  - <h4> Tests: <font color="green">good</font></h4>
   
   - <h4> Documentation: <font color=green>good</font></h4>
   
-    Documentation covers most of the relevant parts of the code and explains how to use it.
+    Documentation establishes more clear wording and provides in depth understanding of the code.
     
 ##average
-
-   - <h4>Specification: </h4>
-   
-   >Given `InputIterator first, last` (which are `InputIterator`s), optionally `T initvalue = {}`,
-   compute the average of the elements denoted by iterator range `[first, last)`, starting the sum from
-   `initvalue`. Does not warn if integer division or similar happens.
    
    - <h4>Code Review: too small for review</h4>
   
--- tests/transform_iterator_tests.cpp
+++ tests/transform_iterator_tests.cpp
@@ -3,35 +3,84 @@
 //
 
 #include "../src/transform_iterator.hpp"
+#include "../src/random_engine.hpp"
 
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <iostream>
+#include <cstddef>
+#include <cassert>
 
-void convert_strings_to_ints()
+template <typename T = int>
+std::vector<T> generate_randomized_vector(std::size_t size)
 {
-    std::vector<std::string> v{"23", "25", "27"}; //just random numbers
-    std::vector<int> output(v.size());
+    std::vector<T> result(size);
 
-    auto string_to_int = [](const std::string& x)
+    static shino::random_int_generator<> gen(0, 100);
+    gen(result.begin(), result.end());
+
+    return result;
+}
+
+template <typename T>
+void multiplication_test(const std::vector<T>& input)
+{
+    std::vector<T> correct_answer(input.size());
+
+    for (std::size_t i = 0; i < input.size(); ++i)
     {
-        return std::stoi(x);
+        correct_answer[i] = input[i] * 2;
+    }
+
+    std::vector<T> transformation_result(input.size()) ;
+
+    auto multiply_by_two = [](const T& value)
+    {
+        return value * 2;
     };
 
-    auto conversion_iterator = shino::transformer(string_to_int, output.begin());
-    std::copy(v.begin(), v.end(), conversion_iterator);
+    std::copy(input.begin(), input.end(),
+              shino::transformer(multiply_by_two, transformation_result.begin()));
 
-    std::cout << "Output after converting v to vector of ints\n";
+    assert(correct_answer == transformation_result);
+}
 
-    for (const auto& element: output)
+template <typename T>
+void stringify_test(const std::vector<T>& input)
+{
+    std::vector<std::string> correct_answer(input.size());
+
+    for (std::size_t i = 0; i < input.size(); ++i)
     {
-        std::cout << element << '\n';
+        correct_answer[i] = "The number is: " + std::to_string(input[i]);
+    }
+
+    std::vector<std::string> transformation_result(input.size());
+
+    std::copy(input.begin(), input.end(),
+              shino::transformer([](const T& value)
+                                 {
+                                     return "The number is: " + std::to_string(value);
+                                 },
+                                 transformation_result.begin()));
+
+    assert(correct_answer == transformation_result);
+}
+
+void run_tests()
+{
+    static shino::random_int_generator<std::size_t> size_gen(100, 10'000);
+
+    constexpr std::size_t run_count = 500;
+    for (std::size_t i = 0; i < run_count; ++i)
+    {
+        multiplication_test(generate_randomized_vector(size_gen()));
+        stringify_test(generate_randomized_vector(size_gen()));
     }
 }
 
 int main()
 {
-    convert_strings_to_ints();
+    run_tests();
 }
 
