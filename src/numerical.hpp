0f1b277a4e99654e772cd685b27ca4c9707fc1c5
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2017-06-29 09:55:25 +0600

    all numerical is documented, including prime numbers from last commit

--- src/numerical.hpp
+++ src/numerical.hpp
@@ -7,6 +7,16 @@
 
 namespace shino
 {
+    /**
+     * Calculates the sum of elements of the sequence and divies by the number of elements
+     * inside it. Effectively computes `std::accumulate()` divided by `std::distance()`.
+     * @tparam ForwardIt A forward iterator type
+     * @tparam T sum type, e.g. object of this type will hold the sum
+     * @param first iterator to the first element of the sequence
+     * @param last iterator to one past the end element of the sequence
+     * @param initvalue initial value of the sum
+     * @return average of all elements in the sequence
+     */
     template <typename ForwardIt, typename T = typename std::iterator_traits<ForwardIt>::value_type>
     T average(ForwardIt first, ForwardIt last, T initvalue = {}) //will be 0 for builtins
     {
